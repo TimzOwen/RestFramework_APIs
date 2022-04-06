b0b33a4ae55402c9fedf59345059e9cfa685ba5e
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-06 22:49:33 +0300

    updated to response from HTTP

--- customers/views.py
+++ customers/views.py
@@ -1,44 +1,43 @@
 from django.http import HttpResponse
 from .serializers import CustomerSerializer
 from .models import Customer
-from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
-@csrf_exempt
+@api_view(['GET','POST'])
 def customer_list(request):
     if request.method == "GET":
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     if request.method == "POST":
         data = JSONParser().parse(request)
         serializer = CustomerSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
 def customer_details(request, pk):
     try:
         customer = Customer.objects.get(pk=pk)
     except Customer.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = CustomerSerializer(customer)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     if request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = CustomerSerializer(customer,data=data)
+        serializer = CustomerSerializer(customer,data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'DELETE':
         customer.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
