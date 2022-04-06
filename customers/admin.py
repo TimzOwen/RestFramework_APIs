e2057786197858ae06d64d6cc18a05d9da367f1b
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-06 16:34:45 +0300

    updated the database

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name','age','address','phone_number']
 
 # Register your models here.
+admin.site.register(Customer, CustomerAdmin)
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    address = models.CharField(max_length=2000)
+    phone_number = models.CharField(max_length=15)
